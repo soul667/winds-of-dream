1aa6f781fc574802b22cc365fd1df226a85e9eba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-08-22 19:30:11 +0000

    Deploy soul667/winds-of-dream to github.com/soul667/winds-of-dream.git:gh-pages

--- assets/index.md.c96feae2.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as n,S as l}from"./chunks/framework.fc296b49.js";const A=JSON.parse('{"title":"使用VitePress和Github搭建个人博客网站","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),e={name:"index.md"},o=l("",22),p=[o];function t(r,c,i,C,y,D){return a(),n("div",null,p)}const h=s(e,[["render",t]]);export{A as __pageData,h as default};
